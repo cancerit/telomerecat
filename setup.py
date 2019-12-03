109ae7a86acddd92987bc09dd98d61e9651efb82
Yaobo Xu <yx2@sanger.ac.uk>
2019-12-03 15:32:09 +0000

    clearer setup req

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 from setuptools import setup, find_packages
-import os
-import sys
-import shutil
 
 from telomerecat.version import __version__
 
@@ -11,10 +8,11 @@ setup(
   version=__version__,
   author="JHR Farmery",
   license="GPL",
+  python_requires': '>= 3.6',
   author_email="cgpit@sanger.ac.uk",
   packages=["telomerecat"],
   package_dir={"telomerecat": "telomerecat"},
-  install_requires=["parabam>=2.2", "argparse", "numpy", "pysam", "pandas"],
+  install_requires=["parabam>=2.2.5", "argparse", "numpy", "pysam", "pandas"],
   include_package_data=True,
   scripts=["./telomerecat/bin/telomerecat"],
   zip_safe=False,
