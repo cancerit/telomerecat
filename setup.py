c7d9e31461e5d34304e4b03aa45f22df096b5c27
Yaobo Xu <yx2@sanger.ac.uk>
2019-12-03 16:53:32 +0000

    added python version restriction, removed argparse as requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
 
+from setuptools import setup
 from telomerecat.version import __version__
 
 setup(
@@ -8,11 +9,11 @@ setup(
   version=__version__,
   author="JHR Farmery",
   license="GPL",
-  python_requires': '>= 3.6',
+  python_requires='>= 3.6',
   author_email="cgpit@sanger.ac.uk",
   packages=["telomerecat"],
   package_dir={"telomerecat": "telomerecat"},
-  install_requires=["parabam>=2.2.5", "argparse", "numpy", "pysam", "pandas"],
+  install_requires=["parabam>=2.2.5", "numpy", "pysam", "pandas"],
   include_package_data=True,
   scripts=["./telomerecat/bin/telomerecat"],
   zip_safe=False,
