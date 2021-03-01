a88f87313a78de290ebb354a69ecf3782c8a0714
Keiran Raine <kr2@sanger.ac.uk>
2021-03-01 10:22:44 +0000

    Ensure current parabam

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ from setuptools import setup
 
 setup(
   name="telomerecat",
-  description="Telomere Computational Analysis Tool",
   version='3.4.1',
+  description="Telomere Computational Analysis Tool",
+  url='https://github.com/cancerit/telomerecat',
   author="JHR Farmery",
   license="GPL",
   python_requires='>= 3.6',
   author_email="cgphelp@sanger.ac.uk",
   packages=["telomerecat"],
   package_dir={"telomerecat": "telomerecat"},
-  install_requires=["parabam>=2.3.0", "numpy", "pysam", "pandas"],
+  install_requires=["parabam>=2.3.2", "numpy", "pysam", "pandas"],
   include_package_data=True,
   scripts=["./telomerecat/bin/telomerecat"],
   zip_safe=False,
